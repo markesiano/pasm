4d29ad73c12d3497a7b3df17b3711b8191e56208
markesiano <markesiano@live.com.mx>
2023-10-26 12:45:14 -0600

    modified:   resources/views/post/index.blade.php (Listado de posts)

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,11 +1,43 @@
 <x-app-layout>
 
 
-    <div class="container">
+    <div class="max-w-7x1 mx-auto px-2 sm:px-6 lg:px-8 py-8">
+
+        <div class="grid grid-cols-3 gap-6">
+            @foreach ($posts as $post)
 
 
 
-        dos
+            <article class="w-full h-80 bg-cover bg-center @if($loop->first) col-span-2 @endif"  style="background-image: url({{ url('storage/' . $post->image->url) }})">
+
+
+                <div class="w-full h-full px-8 flex flex-col justify-center">
+                     <h1 class="text-4xl text-white leading-8 font-bold">
+                        <a href="">
+
+                            {{$post->name}}
+                        </a>
+                     </h1>
+                     <div>
+                        @foreach ($post->tags as $tag)
+
+                            <a href="" class="inline-block px-3 h-6 bg-gray-200 text-gray rounded">
+                                {{$tag->name}}
+                            </a>
+
+                        @endforeach
+
+
+                     </div>
+                </div>
+
+            </article>
+
+        @endforeach
+
+        </div>
+
+
 
 
 
